18f82ade18b71b57612a0a6b1f849da1df702624
Ingolf Wagner <contact@ingolf-wagner.de>
2021-11-13 18:30:15 +0100

    add git-pull-check-all-repositories script

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "home manager utilities";
   inputs.home-manager.url = "github:nix-community/home-manager/release-21.05";
-  outputs = { self, home-manager , ... }: {
+  outputs = { self, home-manager, ... }: {
     hmModules.git-pull = import ./git-pull.nix { hmlib = home-manager.lib; };
     hmModule = {
       imports = [ self.hmModules.git-pull ];
--- git-pull.nix
+++ git-pull.nix
@@ -25,18 +25,31 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.activation = let
-    in {
-      downloadGitRepositories = hmlib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        # clone repositories
-        ${lib.concatStringsSep "\n" (map ({ source, target }: ''
-          if [[ ! -d ${target} ]]
+    home.packages = [
+      # todo : check if repositories are pushed too
+      (pkgs.writers.writeBashBin "git-pull-check-all-repositories" (lib.concatStringsSep "\n" (map
+        ({ target, ... }: ''
+          cd ${target}
+          if [[ $(git status --porcelain | wc -l) -gt 0 ]]
           then
-            $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir -p $( ${pkgs.coreutils}/bin/dirname ${target} )
-            $DRY_RUN_CMD ${pkgs.git}/bin/git clone ${source} ${target}
+            echo "[CHANGED] ${target}"
           fi
-        '') cfg.repositories)}
-      '';
-    };
+        '')
+        cfg.repositories)))
+    ];
+    home.activation =
+      let
+      in {
+        downloadGitRepositories = hmlib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          # clone repositories
+          ${lib.concatStringsSep "\n" (map ({ source, target }: ''
+            if [[ ! -d ${target} ]]
+            then
+              $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir -p $( ${pkgs.coreutils}/bin/dirname ${target} )
+              $DRY_RUN_CMD ${pkgs.git}/bin/git clone ${source} ${target}
+            fi
+          '') cfg.repositories)}
+        '';
+      };
   };
 }
